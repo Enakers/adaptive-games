baa1095a4af911125ca98f071ea8ce3f736d2994
EnakNalla <enak.nalla@gmail.com>
2022-07-24 19:03:02 +0100

    feat(docs): add security headers with next-safe

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,5 +1,3 @@
-import {env} from "./src/server/env.mjs";
-
 /**
  * Don't be scared of the generics here.
  * All they do is to give us autocompletion when using this.
@@ -8,10 +6,33 @@ import {env} from "./src/server/env.mjs";
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
  */
+import nextSafe from "next-safe";
+
 function defineNextConfig(config) {
   return config;
 }
 
 export default defineNextConfig({
-  reactStrictMode: true
+  reactStrictMode: true,
+  output: "standalone",
+  headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          ...nextSafe({
+            isDev: process.env.NODE_ENV !== "production",
+            contentSecurityPolicy: {
+              "script-src": ["'self'"],
+              "img-src": ["'self'", "data:"]
+            }
+          }),
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload"
+          }
+        ]
+      }
+    ];
+  }
 });
